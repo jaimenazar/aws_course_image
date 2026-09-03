5737d4db02335a2c5a4457975477a2452fd4304b
Jaime Nazar Anchorena <j.nazar.anchorena@accenture.com>
2026-09-03 15:43:43 +0000

    feat: app done

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
@@ -12,25 +13,38 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-    /**************************************************************************** */
-
-  //! END @TODO1
   
+  // Filtered image endpoint
+  app.get( "/filteredimage", async (req, res) => {
+
+    const stringIsAValidUrl = (s) => {
+      try {
+        new URL(s);
+        return true;
+      } catch (err) {
+        return false;
+      }
+    };
+
+    if (!req.query.image_url) {
+      return res.status(400).send("Missing required image URL")
+    }
+
+    if (!stringIsAValidUrl(req.query.image_url)) {
+      return res.status(422).send("Invalid image URL")
+    }
+
+    const image_url = req.query.image_url
+    
+    const path = await filterImageFromURL(image_url)
+
+    res.status(200).sendFile(path).then(function(){
+      // Delete image after response
+      fs.unlinkSync(path)
+    });
+
+  } );
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async (req, res) => {
